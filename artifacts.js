4b009d00248903da378f11c4fa59c1a0221bd722
TuitionSucks <heyimjamesparkii@gmail.com>
2026-08-25 14:25:30 -0700

    Add Chilly artifact data

---
+++ artifacts.js
@@ -0,0 +1,22 @@
+const ARTIFACTS = {
+  chilly: {
+    name: "Chilly",
+    verifiedQualityBand: { min: 130, max: 145, rarity: "Rare" },
+    main: {
+      vitality: 2.70,
+      temperature: -0.384,
+      burning: -0.40
+    },
+    frost: {
+      qualityMin: 130,
+      qualityMax: 145,
+      valueAtMin: 0.85,
+      valueAtMax: 1.00
+    },
+    additional: {
+      vitality: 0.90,
+      burning: -0.20,
+      explosionProtection: 14.90
+    }
+  }
+};
